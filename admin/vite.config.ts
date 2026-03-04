6109898e95403b0e9c71e30c720dc62ab4b7754f
MkRoD Sullivan <mkrodsullivan@gmail.com>
2026-03-04 16:01:47 +0100

    Admin start

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path";
-import { readFileSync } from 'fs';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { readFileSync, existsSync } from 'fs';
 
-// https://vitejs.dev/config/
+const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
+    },
   },
   server: {
     host: true,
-    https: {
-      key: readFileSync(path.resolve(__dirname, "./keys/localhost+1-key.pem")),
-      cert: readFileSync(path.resolve(__dirname, "./keys/localhost+1.pem")),
-    },
     port: 5180,
+    ...(isDev && existsSync(path.resolve(__dirname, "./keys/localhost+1-key.pem")) && {
+      https: {
+        key: readFileSync(path.resolve(__dirname, "./keys/localhost+1-key.pem")),
+        cert: readFileSync(path.resolve(__dirname, "./keys/localhost+1.pem")),
+      },
+    }),
   },
 });
