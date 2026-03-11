686750b07fd79e816ce1d248e042054444d8d1ec
MkRoD Sullivan <mkrodsullivan@gmail.com>
2026-03-11 18:57:22 +0100

    Updates

--- server/routes/post.routes.ts
+++ server/routes/post.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { addPost, deletePost, getPosts, getSpecificPost } from "../controllers/post.controller.js";
-import { authenticateRoute, requireAuth } from "../middlewares/auth.js";
+import { authenticateRoute, optionalAuth, requireAuth } from "../middlewares/auth.js";
 const router: Router = Router();
 
 
-router.get("/", getPosts);
+router.get("/", optionalAuth, getPosts);
 router.get("/:slug", getSpecificPost);
 router.put("/", requireAuth, authenticateRoute({ requireUser: true, role: "admin" }), addPost);
 router.delete("/", requireAuth, authenticateRoute({ requireUser: true, role: "admin" }), deletePost);
